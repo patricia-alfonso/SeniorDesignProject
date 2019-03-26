07619a4b3157f2ebefb356e2b229b3995ab5159c
alden-richter <aldenrichter97@gmail.com>
2019-03-26 12:25:45 -0400

    Rebuilt app using only node/jquery

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
-const {app, BrowserWindow} = require('electron')
+const electron = require('electron')
+const { app, BrowserWindow } = electron
+const { ipcMain } = require('electron')
 
 let win
 
 app.on('ready', () =>{
-    win = new BrowserWindow({width:800, height:600, show: false})
+    const { width, height } = electron.screen.getPrimaryDisplay().workAreaSize
+    win = new BrowserWindow({
+        width: width / 2, 
+        height: height * 3 / 4, 
+        show: false,
+        frame: false
+    })
 
     win.loadFile('index.html')
+
+    // DEV ONLY
+    win.toggleDevTools();
 
     win.once('ready-to-show', () => {
         win.show()
@@ -14,4 +25,8 @@ app.on('ready', () =>{
     win.on('closed', () => {
         win = null
     })
+})
+
+ipcMain.on('quit', () => {
+    app.quit()
 })
